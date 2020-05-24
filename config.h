8512bc337179275b7bbd58098a2489f4ad204e51
Martin Homuth <martin.homuth@emlix.com>
2020-05-24 19:45:53 +0200

    add fontawesome and android studio rules
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

--- config.h
+++ config.h
@@ -14,7 +14,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char *fonts[]          = { "Mono:size=10:antialias=true:autohint=true", "Font Awesome 5 Free:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -45,7 +45,9 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1,            0,           -1 },
-	{ "KeePass2", NULL,       NULL,       0,            1,           -1 },
+	{ "KeePassXC",NULL,       NULL,       0,            1,           -1 },
+	{ NULL,       NULL, "Welcome to Android Studio", 0, 1,           -1 },
+	{ NULL,       NULL, "Android Studio Setup Wizard", 0, 1,         -1 },
 };
 
 /* layout(s) */
