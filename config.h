40a63ad2b754df214f448277ff97a0a2e4be1cbd
Martin Homuth <mh@emlix.com>
2020-09-24 19:21:26 +0200

    add more fonts
    Signed-off-by: Martin Homuth <mh@emlix.com>

--- config.h
+++ config.h
@@ -22,7 +22,13 @@ static const int smartgaps          = 0;        /* 1 means no outer gap when
 						   there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Mono:size=10:antialias=true:autohint=true", "Font Awesome 5 Free:size=10" };
+static const char *fonts[]          = {
+	"Mono:size=10:antialias=true:autohint=true",
+	"Font Awesome 5 Free:size=10",
+	"Hack:size=10",
+	"FontAwesome:size=10",
+	"Siji:size=10",
+};
 static const char dmenufont[]       = "monospace:size=10";
 static const char normbgcolor[]           = "#282828";
 static const char normbordercolor[]       = "#504945";
