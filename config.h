62743133e307f2749ac4d868976dad086cb3020c
Martin Homuth <mh@emlix.com>
2021-03-14 07:44:21 +0100

    pin systray to monitor 1
    Signed-off-by: Martin Homuth <mh@emlix.com>

--- config.h
+++ config.h
@@ -5,7 +5,7 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int swallowfloating    = 0;        /* 1 means swallow floating
 						   windows by default */
-static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows
+static const unsigned int systraypinning = 1;   /* 0: sloppy systray follows
 						   selected monitor, >0: pin
 						   systray to monitor X */
 static const unsigned int systrayspacing = 7;   /* systray spacing */
