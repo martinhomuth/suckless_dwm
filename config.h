90e4147e2ba1351cb272e85267c8a23db0b2f421
Martin Homuth <martin@followthestack.tech>
2021-12-23 09:04:36 +0100

    add guard against color fonts
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- config.h
+++ config.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#define INHIBIT_COLOR_FONTS 0
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
