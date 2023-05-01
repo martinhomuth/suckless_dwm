aa2f6ffbaec72faf4e83f125fd0540a11151c9f1
Martin Homuth <martin@followthestack.tech>
2023-05-01 19:18:03 +0200

    add actualfullscreen patch
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- dwm.c
+++ dwm.c
@@ -200,6 +200,7 @@ static void sendmon(Client *c, Monitor *m);
 static void setclientstate(Client *c, long state);
 static void setfocus(Client *c);
 static void setfullscreen(Client *c, int fullscreen);
+static void fullscreen(const Arg *arg);
 static void setlayout(const Arg *arg);
 static void setmfact(const Arg *arg);
 static void setup(void);
@@ -1516,6 +1517,15 @@ setfullscreen(Client *c, int fullscreen)
 		resizeclient(c, c->x, c->y, c->w, c->h);
 		arrange(c->mon);
 	}
+}
+
+Layout *last_layout;
+
+void
+fullscreen(const Arg *arg)
+{
+	if(selmon->sel)
+		setfullscreen(selmon->sel, !selmon->sel->isfullscreen);
 }
 
 void
