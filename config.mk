f1bebe0c40b664eff5fd587effdf45f55850f481
Martin Homuth <martin@followthestack.tech>
2024-08-26 21:47:40 +0200

    makefile: use std=c2x instead of std=c23
    Gentoo currently uses gcc13 which means only std=c2x is supported. gcc14 does
    support that flag as well even though it is deprecated. With this we should be
    fine in all used systems.
    
    Signed-off-by: Martin Homuth <martin@followthestack.tech>

--- config.mk
+++ config.mk
@@ -28,7 +28,7 @@ LIBS = -L${X11LIB} -lX11 ${XINERAMALIBS} ${FREETYPELIBS} -lX11-xcb -lxcb -lxcb-r
 
 # flags
 CPPFLAGS = -D_DEFAULT_SOURCE -D_BSD_SOURCE -D_XOPEN_SOURCE=700L -DVERSION=\"${VERSION}\" ${XINERAMAFLAGS}
-CFLAGS   = -std=c23 -pedantic -Wall -Werror -Wno-deprecated-declarations -fanalyzer -fsanitize=address -Os ${INCS} ${CPPFLAGS}
+CFLAGS   = -std=c2x -pedantic -Wall -Werror -Wno-deprecated-declarations -fanalyzer -fsanitize=address -Os ${INCS} ${CPPFLAGS}
 LDFLAGS  = ${LIBS} -fsanitize=address
 
 # Solaris
