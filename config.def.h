7ad7a9fb42520ef54d9192b6ec64402de6b9fc4d
Martin Homuth <martin.homuth@emlix.com>
2024-09-15 17:33:08 +0200

    config: update dmenu colors
    Signed-off-by: Martin Homuth <martin.homuth@emlix.com>

--- config.def.h
+++ config.def.h
@@ -88,7 +88,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", gray3, "-sb", yellow, "-sf", black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", gray3, "-sb", yellow, "-sf", black, "-c", NULL };
 static const char *termcmd[]  = { TERMINAL, NULL };
 static const char scratchpadname[] = "emacs-scratchpad";
 static const char *scratchpadcmd[] = { "emacs", "-T", scratchpadname, NULL };
